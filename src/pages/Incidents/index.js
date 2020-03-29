c2b7bfaa0be0d051a37dd42431eba46f55e292e2
João Fanchini <joaofanchini@gmail.com>
2020-03-29 19:09:32 -0300

    Aplicação de estilo na tela de incidentes

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
 import logo from '../../../assets/logo.png'; // Já importa no melhor formato
 import styles from './style';
+
+// É utilizado para linkar activities criadas, necessário instanciar
+import { useNavigation } from '@react-navigation/native';
 
 import { Feather } from '@expo/vector-icons';
 
@@ -19,7 +22,30 @@ import { Feather } from '@expo/vector-icons';
  * um efeito de opacidade no local.
  */
 
+/**
+ * Para listar algo, devemos utilizar o elemento FlatList quando queremos listar itens,
+ * "o que permite o efeito de scroll"
+ *
+ * O FlatList consiste em ser um elemento de chaves único que através de alguns atributos realiza
+ * a listagem de elementos:
+ *  data => Consiste em ser o array de elementos que irá receber
+ *  keyExtractor => Consiste em ser qual propriedade que indica a unicidade entre os elementos,
+ *  recebe como elemento uam String, como a chave de teste pode ser um número, você pode executar uma função tratamento o parâmetro para uma string
+ *  style => Função de estilo
+ *  renderItem => O que será renderizado por cada elemento da lista (lembrando que quando
+ *  passamos entre parenteses, estamos retornando um JSX.) que recebe uma função que retorna
+ * JSX.
+ *
+ *  Passando a propriedade showsVerticalScrollIndicator conseguimos retirar a barra ao lado de scroll
+ */
+
 export default function Incident() {
+  const navigation = useNavigation();
+
+  function toDetails() {
+    navigation.navigate('Details'); // Same name defined on the routes
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -34,36 +60,27 @@ export default function Incident() {
         Escolha um dos casos abaixo e salve o dia
       </Text>
 
-      <View style={styles.incidentsList}>
-        <View style={styles.incident}>
-          <Text style={styles.incidentProperty}>ONG:</Text>
-          <Text style={styles.incidentProperty}>APAD</Text>
+      <FlatList
+        data={[1, 2, 3]}
+        style={styles.incidentsList}
+        keyExtractor={incident => String(incident)}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentProperty}>APAD</Text>
 
-          <Text style={styles.incidentProperty}>CASO:</Text>
-          <Text style={styles.incidentProperty}>Cadelinha atropelada</Text>
+            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentProperty}>Cadelinha atropelada</Text>
 
-          <Text style={styles.incidentProperty}>VALOR:</Text>
-          <Text style={styles.incidentProperty}>R$ 120,00</Text>
-          <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
-            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-            <Feather name='arrow-right' size={23} color='#E02041'></Feather>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.incident}>
-          <Text style={styles.incidentProperty}>ONG:</Text>
-          <Text style={styles.incidentProperty}>APAD</Text>
-
-          <Text style={styles.incidentProperty}>CASO:</Text>
-          <Text style={styles.incidentProperty}>Cadelinha atropelada</Text>
-
-          <Text style={styles.incidentProperty}>VALOR:</Text>
-          <Text style={styles.incidentProperty}>R$ 120,00</Text>
-          <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
-            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-            <Feather name='arrow-right' size={23} color='#E02041'></Feather>
-          </TouchableOpacity>
-        </View>
-      </View>
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentProperty}>R$ 120,00</Text>
+            <TouchableOpacity style={styles.detailsButton} onPress={toDetails}>
+              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Feather name='arrow-right' size={23} color='#E02041'></Feather>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
